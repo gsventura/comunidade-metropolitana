1b2d3b981085f4e9c9faf6e04f3fca63f158253c
gsventura <scholl.ventura@gmail.com>
2025-04-10 09:36:15 -0300

    v8 commit

--- src/app/admin/edit/[id]/page.tsx
+++ src/app/admin/edit/[id]/page.tsx
@@ -3,14 +3,12 @@
 
 import { EditarAnuncioClient } from './client';
 
-// Esta é a tipagem correta para páginas dinâmicas no Next.js App Router
-type Props = {
-  params: { id: string }
-  searchParams: { [key: string]: string | string[] | undefined }
-}
-
 // Este é o Server Component que recebe os params
-export default function EditarAnuncioPage({ params }: Props) {
+export default async function EditarAnuncioPage({
+  params,
+}: {
+  params: { id: string };
+}) {
   // Extrair o ID do parâmetro
   const anuncioId = params.id;
   
